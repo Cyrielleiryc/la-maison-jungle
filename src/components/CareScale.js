174276e2c77a0d267df034e5d56ac72363adc357
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-10-12 17:54:21 +0200

    alert for click on care 'water' or 'light'

--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -1,6 +1,19 @@
 import Sun from '../assets/sun.svg'
 import Water from '../assets/water.svg'
 
+function handleClick(scaleValue, careType) {
+  const need = `${careType === 'water' ? "d'arrosage" : 'de lumière'}`
+  let adv
+  if (scaleValue === 1) {
+    adv = 'peu'
+  } else if (scaleValue === 2) {
+    adv = 'modérement'
+  } else {
+    adv = 'beaucoup'
+  }
+  alert(`Cette plante requiert ${adv} ${need}`)
+}
+
 function CareScale({ scaleValue, careType }) {
 	const range = [1, 2, 3]
 	const scaleType =
@@ -11,7 +24,7 @@ function CareScale({ scaleValue, careType }) {
 		)
 
 	return (
-		<div>
+		<div onClick={() => handleClick(scaleValue, careType)}>
 			{range.map((rangeElem) =>
 				scaleValue >= rangeElem ? (
 					<span key={rangeElem.toString()}>{scaleType}</span>
