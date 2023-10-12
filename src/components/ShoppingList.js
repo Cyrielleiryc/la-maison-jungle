69eef293eee4f46041657c715645bb43e8e435c8
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-10-12 16:03:06 +0200

    added children component 'PlantItem'

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,29 +1,30 @@
-import { plantList } from '../datas/plantList.js'
+import { plantList } from '../datas/plantList'
+import PlantItem from './PlantItem'
 import '../styles/ShoppingList.css'
-import CareScale from './CareScale.js'
 
 function ShoppingList() {
-  const categories = plantList.reduce(
+	const categories = plantList.reduce(
 		(acc, plant) =>
 			acc.includes(plant.category) ? acc : acc.concat(plant.category),
 		[]
 	)
 
-  return (
-		<div className="lmj-plant-list">
+	return (
+		<div>
 			<ul>
 				{categories.map((cat) => (
 					<li key={cat}>{cat}</li>
 				))}
 			</ul>
-			<ul>
-				{plantList.map((plant) => (
-					<li key={plant.id} className="lmj-plant-item">
-            {plant.name}
-            <CareScale careType='water' scaleValue={plant.water} />
-            <CareScale careType='light' scaleValue={plant.light} />
-            {plant.isSpecialOffer && <div className="lmj-sales">Soldes</div>}
-          </li>
+			<ul className='lmj-plant-list'>
+				{plantList.map(({ id, cover, name, water, light }) => (
+					<PlantItem
+						id={id}
+						cover={cover}
+						name={name}
+						water={water}
+						light={light}
+					/>
 				))}
 			</ul>
 		</div>
